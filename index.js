bb2c5d67914f580db4aa678582f3f9f00d812d28
Hafedh Ben Haj Ali <70435711+hafedhBel@users.noreply.github.com>
2023-10-10 04:46:35 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-var generateName = require('sillyname');
+//var generateName = require('sillyname');
+ import generateName from "sillyname"
 var sillyName = generateName();
 console.log(`my name is ${sillyName}.`);
